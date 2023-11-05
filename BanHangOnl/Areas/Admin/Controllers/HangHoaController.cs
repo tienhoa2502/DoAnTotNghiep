5d798294b128fd12b5b6149ccf43e6d29131b830
Uy Võ Trần <votranuy2001@gmail.com>
2023-11-05 23:57:37 +0700

    Uy update 05/11/2023, lưu ảnh

--- BanHangOnl/Areas/Admin/Controllers/HangHoaController.cs
+++ BanHangOnl/Areas/Admin/Controllers/HangHoaController.cs
@@ -1,5 +1,7 @@
 ﻿using BanHangOnl.Models;
+using Humanizer.Localisation.TimeToClockNotation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BanHangOnl.Areas.Admin.Controllers
 {
@@ -7,11 +9,64 @@ namespace BanHangOnl.Areas.Admin.Controllers
     public class HangHoaController : Controller
     {
         QuanLyNhaHangContext context = new QuanLyNhaHangContext();
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public HangHoaController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         [HttpGet("/HangHoa")]
         public IActionResult Index()
         {
             ViewBag.HangHoa = context.HangHoas.Where(x => x.Active == true).ToList();
             return View();
+        }
+        [HttpGet("/HangHoa/Them")]
+        public IActionResult ViewAdd()
+        {
+            return View("Add");  
+        }
+        [HttpPost("/HangHoa/updateHangHoa")]
+        public dynamic addHangHoa(HangHoa hangHoa) {
+            return new
+            {
+                id = 1,
+            };
+        }
+        [HttpPost("/HangHoa/UpdateAnh")]
+        public IActionResult UpdateHanHoa([FromForm] List<IFormFile> files, [FromQuery] int idHangHoa)
+        {
+            HangHoa hangHoa = context.HangHoas.Find(idHangHoa);
+            var listAnhCu = context.ImgHangHoas.Where(x => x.Idhh == idHangHoa).ToList();   
+            if(listAnhCu.Count()> 0)
+            {
+                context.ImgHangHoas.RemoveRange(listAnhCu);
+                context.SaveChanges();  
+            }
+            List<ImgHangHoa> imgHangHoas = new List<ImgHangHoa>();
+            int i = 1;
+            foreach (IFormFile formFile in files)
+            {
+                ImgHangHoa img = new ImgHangHoa();  
+                string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "img/HangHoa");
+                string uniqueFileName = hangHoa.MaHh + " " + i + Path.GetExtension(formFile.FileName);
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                // Lưu file ảnh vào thư mục /assets/img/avatars/
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    formFile.CopyTo(fileStream);
+                }
+                i++;
+                img.Img = "/img/HangHoa/" + uniqueFileName;
+                img.Idhh = idHangHoa;
+                img.IsDefault = true;
+                imgHangHoas.Add(img);
+            }
+            context.ImgHangHoas.AddRange(imgHangHoas);
+
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ BanHangOnl/wwwroot/jsAdmin/HangHoa.js
@@ -0,0 +1,35 @@
+﻿function UpdateHangHoa() {
+    var form = $('#formThemHH');
+    var formData = form.serialize();
+    $.ajax({
+        url: "/HangHoa/updateHangHoa",
+        type: "POST",
+        data: formData,
+        success: function (response) {
+            const files = $("#fileInput")[0].files;
+            const fileFormData = new FormData(); // Đặt tên biến khác
+
+            // Thêm các tệp đã chọn vào FormData
+            for (let i = 0; i < files.length; i++) {
+                fileFormData.append("files", files[i]);
+            }
+
+            $.ajax({
+                url: "/HangHoa/UpdateAnh?idHangHoa=" + response.id,
+                type: "POST",
+                data: fileFormData, // Sử dụng biến khác
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    window.location.href = "/HangHoa";
+                },
+                error: function (error) {
+                    console.error("Lỗi khi cập nhật hàng hóa: " + error);
+                }
+            });
+        },
+        error: function (error) {
+            console.error("Lỗi khi cập nhật hàng hóa: " + error);
+        }
+    });
+}
